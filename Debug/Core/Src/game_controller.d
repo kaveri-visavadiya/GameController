2f161803f73602deabb0b132f875ce0050ca4da5
KaveriVisavadiya <145709121+KaveriVisavadiya@users.noreply.github.com>
2025-07-08 01:05:51 +0530

    Add files via upload

---
+++ Debug/Core/Src/game_controller.d
@@ -0,0 +1,3 @@
+Core/Src/game_controller.o: ../Core/Src/game_controller.c \
+ ../Core/Inc/game_controller.h
+../Core/Inc/game_controller.h:
--- Debug/Core/Src/main.d
+++ Debug/Core/Src/main.d
@@ -34,7 +34,7 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pcd_ex.h \
  ../USB_DEVICE/App/usb_device.h \
  ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
- ../USB_DEVICE/Target/usbd_conf.h
+ ../USB_DEVICE/Target/usbd_conf.h ../Core/Inc/game_controller.h
 ../Core/Inc/main.h:
 ../Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
 ../Core/Inc/stm32f4xx_hal_conf.h:
@@ -72,3 +72,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../USB_DEVICE/App/usb_device.h:
 ../Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
 ../USB_DEVICE/Target/usbd_conf.h:
+../Core/Inc/game_controller.h:
